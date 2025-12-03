cb4f38b593b06327c043044385c10053c2933292
EmilieHolmberg <emilie.b.holmberg@hotmail.dk>
2025-12-03 16:19:18 +0100

    Added comment for reference to what i've learned about TS

--- 2024/dec1/part2.ts
+++ 2024/dec1/part2.ts
@@ -3,7 +3,7 @@ import * as fs from 'fs';
 const file: string = fs.readFileSync("2024/input/dec1.txt", 'utf-8');
 const lines: string[] = file.split("\n");
 
-lines.forEach((line, index) => {
+lines.forEach((line, index) => { //Remove \r from each line
   lines[index] = line.replace(/\r/g, '');
 })
 
@@ -21,10 +21,10 @@ for (const line of lines){
 // console.log("Right Row:", rRow);
 
 let total: number = 0;
-for (const lItem of lRow) {
+for (const lItem of lRow) { //Similar to Python's: for lItem in lRow
   let count = 0;
-  rRow.forEach((rItem) => {
-    if (lItem == rItem) count++;
+  rRow.forEach((rItem) => { //Goes though each rItem in rRow
+    if (lItem == rItem) count++; //Similar to python's: count += 1 if lItem == rItem
   })
   total += lItem * count;
 
